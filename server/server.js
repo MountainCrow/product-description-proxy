c5b9531b107ca812e25c0563346a21e1f4908da7
lmodin <lisamodin@live.com>
2020-07-21 16:41:57 -0700

    Updating for our instances 7/21

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ app.use(cors());
 //app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
-
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  });
 app.use(express.static(path.join(__dirname, '../dist')))
 
 app.get('/', (req, res) => {
